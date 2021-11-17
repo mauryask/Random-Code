2eb7cb4c54d2bca84e7bf3e33e7bdbce55c10e23
shubhammaurya <pnssoftwares7@gmail.com>
2021-11-18 01:33:49 +0530

    Upadted

--- Amazon/SortLinkedListAlreadySortedWithAbsoluteValues.java
+++ Amazon/SortLinkedListAlreadySortedWithAbsoluteValues.java
@@ -31,22 +31,22 @@ public class SortLinkedListAlreadySortedWithAbsoluteValues
 		if(head == null)
 			return head;
 		
-		Node ptr = head;
-		Node temp = head.next;
+		Node prev = head;
+		Node ptr = head.next;
 		
-		while(ptr != null && ptr.next != null)
+		while(ptr != null)
 		{
-			 if(temp.data < 0)
+			 if(ptr.data < 0)
 			 {
-				 ptr.next = temp.next;
-				 temp.next = head;
-				 head = temp;
-				 temp = ptr.next;
+				 prev.next = ptr.next;
+				 ptr.next = head;
+				 head = ptr;
+				 ptr = prev.next;
 			 }
 			 else
 			 {
-				 ptr = ptr.next;
-				 temp = ptr.next;
+				 prev = prev.next;
+				 ptr = prev.next;
 			 }
 		}
 		
@@ -55,18 +55,18 @@ public class SortLinkedListAlreadySortedWithAbsoluteValues
 	
 	public static void main(String [] args)
 	{
-	    Node head = new Node(-1);	
-	    Node n2 = new Node(4);	
-	    Node n3 = new Node(-5);	
-	    /*Node n4 = new Node(4);	
+	    Node head = new Node(1);	
+	    Node n2 = new Node(-2);	
+	    Node n3 = new Node(-3);	
+	    Node n4 = new Node(4);	
 	    Node n5 = new Node(-5);	
-	    Node n6 = new Node(-6);*/
+	    Node n6 = new Node(6);
 
 		head.next = n2;
 		n2.next = n3;		
-		/*n3.next = n4;		
+		n3.next = n4;		
 		n4.next = n5;		
-		n5.next = n6;	*/
+		n5.next = n6;	
 
         printList(head);	
         printList(sort(head));		
