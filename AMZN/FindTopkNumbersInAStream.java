61ee9177325ad3864a011e24fa880a8375f96689
mauryask <mauryask1732@gmail.com>
2021-12-21 00:09:44 +0530

    Updated

--- AMZN/FindTopkNumbersInAStream.java
+++ AMZN/FindTopkNumbersInAStream.java
@@ -13,31 +13,38 @@ public class FindTopkNumbersInAStream
    {
        Map<Integer, Integer> map = new HashMap<>();
        
-       for(int x: A)
-           map.put(x, map.getOrDefault(x, 0)+1);
-       
        Queue<Integer> q = new PriorityQueue<>((a, b)->{
-         return map.get(b) - map.get(a);
+         return map.get(a) - map.get(b);
        });
 
        for(int x: A)
-       {
-           q.add(x);           
+       { 
+           map.put(x, map.getOrDefault(x, 0)+1);     
+           if(q.contains(x))
+              q.remove(x);
+          
+           q.add(x);       
+           
            if(q.size() > k)
             q.remove();
+        
            printHeap(q);
        }
    }
    
    static void printHeap(Queue<Integer> q)
    {
-       for(int x : q)
-           out.print(x+" ");
+       Stack<Integer> stack = new Stack<>();
+       for(int x: q)
+           stack.push(x);
+       while(!stack.isEmpty())
+           out.print(stack.pop()+" ");
+       out.println();
    }
    
   public static void main(String [] args)
   {
-	  int[] A = {5, 2, 1, 3, 4};
+	  int[] A = {5, 2, 1, 3, 2};
       int n = A.length;
       int k = 4;
       solve(A, n, k);
