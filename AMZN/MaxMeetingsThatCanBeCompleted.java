753de4d1d652322fd2ac037ed67b2202a102b17e
mauryask <mauryask1732@gmail.com>
2022-03-14 14:51:00 +0530

    Updated

--- AMZN/MaxMeetingsThatCanBeCompleted.java
+++ AMZN/MaxMeetingsThatCanBeCompleted.java
@@ -1,15 +1,22 @@
 import static java.lang.System.*;
 import java.util.*;
 
+/*
+* T(n) : O(n*logn)
+* S(n) : O(n)
+*/
+
 class Meeting 
 {
 	int start;
 	int end;
+	int index;
 	
-	Meeting(int start,int end)
+	Meeting(int start,int end,int index)
 	{
 	   this.start = start;
 	   this.end = end;	   
+	   this.index = index;
 	}
 }
 
@@ -18,41 +25,33 @@ public class MaxMeetingsThatCanBeCompleted
 	static int maxMeets(int st[], int et[], int n)
 	{
 		List<Meeting> list = new ArrayList<>(); 
+		
 		for(int i= 0; i<n; i++)
-			list.add(new Meeting(st[i], et[i]));
+			list.add(new Meeting(st[i], et[i], i));
 		
-		Collections.sort(list, (a, b
-		)->{
-			if(a.end ==  b.end)
-			   return a.start - b.start;
-		     else 
-				 return a.end - b.end;
-		});
-		
-		int i = 0; 
-		int j = 1;
-        int maxMeet = 0;		
-		
+		Collections.sort(list, (a, b) -> (a.end - b.end));
+	        
 		List<Meeting> result = new ArrayList<>();
+		
+		result.add(list.get(0));
+		
+		int lastMeetEnd = list.get(0).end;
 				
-		while(i<n && j<n)
+		for(int i=1; i<n; i++)
 		{
-			if(list.get(i).end < list.get(j).start)
+			if(list.get(i).start > lastMeetEnd)
 			{
-				maxMeet++;
 				result.add(list.get(i));
-				i = j;
-				j++;
+				lastMeetEnd = list.get(i).end;
 			}
-			else
-				j++;
 		}
-				
-        result.add(list.get(i));		
+		
 		for(Meeting m : result)
 			out.println(m.start+"., "+m.end);
-		
-		return maxMeet+1;
+
+		// total meets that can be completed 
+		// is the size of the result list
+		return result.size();
 	}
 	
 	public static void main(String [] args)
